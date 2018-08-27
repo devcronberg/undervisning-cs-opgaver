56bad996bb994c3d90534ecb126bacf944b26945
Michell Cronberg <admin@TA-8215-INS>
2018-08-27 15:29:58 +0200

    Arrays

---
+++ arrays-simpel/Opgave/index.md
@@ -0,0 +1,17 @@
+﻿# Arrays (Simpel)
+
+Du skal oprette et (int) array med et antal tilfældige månedslønninger, udskrive disse samt et beregne et gennemsnit.
+
+```
+10000
+20000
+15000
+20000
+30000
+15000
+Gennemsnit 18.333,33
+```
+
+Prøv eventuelt også at sortere og udskrive.
+
+PS: Spørg evt. instruktør for en mere avanceret løsning
---
+++ arrays-simpel/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_simpel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] månedsløn = { 10000, 20000, 15000, 20000, 30000, 15000 };
+            double sum = 0;
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+                sum += månedsløn[i];
+            }
+            double gns = sum / månedsløn.Length;
+
+            for (int i = 0; i < månedsløn.Length; i++)
+            {
+                Console.WriteLine(månedsløn[i]);
+            }
+            Console.WriteLine("Gennemsnit " + gns.ToString("N2"));
+
+            // Hold console åben ved debug
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
