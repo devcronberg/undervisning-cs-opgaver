ab2e931dd32045ce2bb53fcbdb29aa144fa5b566
Michell Cronberg <admin@TA-8215-INS>
2018-08-28 10:39:16 +0200

    metode

---
+++ Metoder-statisk/Opgave/index.md
@@ -0,0 +1,15 @@
+﻿# Metoder (statisk)
+
+For at lege lidt med instans- og statiske metoder så prøv at kigge på System.IO.File og System.IO.FileInfo.
+
+* Opret en fil på c:\temp kaldet test.txt og put lidt tekst i
+* Dan en ny consol app
+* Skriv kode der benytter (hjælpe)metoder fra System.File
+  - Prøv Exists (findes filen)
+  - Prøv ReadAllText (henter tekst fra filen)
+* Skriv kode der opretter et objekt der repræsenterer filen (System.FileInfo) 
+  * var fil = new System.IO.FileInfo(@"c:\temp\test.txt");
+  * Udskriv LastAccessTime 
+  * Udskriv Extension
+
+Bemærk, at de statiske (hjælpe)metoder kan findes på System.File og instans metoderne kræver at du opretter et objekt og kalder metoderne på selve objektet
---
+++ Metoder-statisk/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metoder_statisk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool t = System.IO.File.Exists(@"c:\temp\test.txt");
+            Console.WriteLine($"Findes fil: {t}");
+            string indhold = System.IO.File.ReadAllText(@"c:\temp\test.txt");
+            Console.WriteLine(indhold);
+
+            var fil = new System.IO.FileInfo(@"c:\temp\test.txt");
+            DateTime d = fil.LastAccessTime;
+            Console.WriteLine($"Tid: {d}");
+            string ex = fil.Extension;
+            Console.WriteLine($"Extension: {ex}");
+
+            
+        }
+    }
+}
