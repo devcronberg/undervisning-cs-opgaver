62b0a3971d24cfcc6de737e9216212f4ed3ffdf7
Michell Cronberg <admin@TA-8215-INS>
2018-08-28 15:05:34 +0200

    statisk

---
+++ klasser-statisk/Opgave/index.md
@@ -0,0 +1,8 @@
+﻿# Klasser (Statisk)
+
+Du skal skabe en tom konsol-applikation med en enkelt klasse kaldet Beregninger. I den skal der være to statiske metoder:
+
+* int LægSammen(int a, int b) der lægger to tal sammen
+* int TrækFra(int a, int b) der trækker to tal fra hinanden
+
+Man må _ikke_ kunne skabe en instans af klassen (privat constructor).
---
+++ klasser-statisk/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasser_statisk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Beregninger b = new Beregninger(); fejl
+            int res = Beregninger.LægSammen(1, 1);
+            Console.WriteLine(res);
+
+            // Hold console åben ved debug
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+
+    public class Beregninger {
+        public static int LægSammen(int a, int b) {
+            return a + b;
+        }
+        public static int TrækFra(int a, int b)
+        {
+            return a - b;
+        }
+
+        private Beregninger()
+        {
+
+        }
+    }
+}
