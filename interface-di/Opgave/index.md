c7253a095c800880d56f49abfe5e77a7a7608ccd
Michell Cronberg <admin@TA-8215-INS>
2018-08-28 12:40:07 +0200

    di

---
+++ interface-di/Opgave/index.md
@@ -0,0 +1,34 @@
+﻿# Interface (Dependency Injection)
+
+Du skal skabe en tom konsol app med et interface kaldet ITilfældighedsGenerator. Interface’et beskriver en enkelt metode kaldet FindTalTilTerning, som skal returnere en int.
+
+Skab nu to klasser der implementere interface’et:
+* TilfældighedsGeneratorFramework
+* TilfældighedsGeneratorMock
+
+Førstnævnte benytter en privat statisk instans af System.Random til at skabe et tilfældigt tal mellem 1 og 6. Sidstnævnte indeholder en custom constructor hvor et tal kan angives, og dette tal returneres blot af FindTalTilTerning-metoden.
+
+Test begge klasser:
+
+```csharp
+ITilfældighedsGenerator m = new TilfældighedsGeneratorMock(1);
+Console.WriteLine(m.FindTalTilTerning());
+ITilfældighedsGenerator f = new TilfældighedsGeneratorFramework();
+Console.WriteLine(f.FindTalTilTerning());
+```
+
+Skab nu en klasse Terning med egenskaben Værdi og en Ryst-metode. Ryst-metoden skal bruge et objekt der implementerer ITilfældighedsGenerator til at finde det tilfældige tal, og objektet skal tildeles Terning i en custom constructor.
+
+Test klassen:
+
+```csharp
+TilfældighedsGeneratorMock m = new TilfældighedsGeneratorMock(1);
+Terning t1 = new Terning(m);
+Console.WriteLine(t1.Værdi);
+
+TilfældighedsGeneratorFramework f = new TilfældighedsGeneratorFramework();
+Terning t2 = new Terning(f);
+Console.WriteLine(t2.Værdi);
+```
+
+Du har nu skabt en (fattigmands)implementation af (konstruktor baseret) dependency injection.
---
+++ interface-di/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        {
+            ITilfældighedsGenerator m = new TilfældighedsGeneratorMock(1);
+            Console.WriteLine(m.FindTalTilTerning());
+            ITilfældighedsGenerator f = new TilfældighedsGeneratorFramework();
+            Console.WriteLine(f.FindTalTilTerning());
+        }
+
+        {
+            TilfældighedsGeneratorMock m = new TilfældighedsGeneratorMock(1);
+            Terning t1 = new Terning(m);
+            Console.WriteLine(t1.Værdi);
+
+            TilfældighedsGeneratorFramework f = new TilfældighedsGeneratorFramework();
+            Terning t2 = new Terning(f);
+            Console.WriteLine(t2.Værdi);
+        }
+
+    }
+}
+
+public interface ITilfældighedsGenerator
+{
+    int FindTalTilTerning();
+}
+
+public class TilfældighedsGeneratorFramework : ITilfældighedsGenerator
+{
+    private static System.Random rnd = new System.Random();
+
+    public int FindTalTilTerning()
+    {
+        return rnd.Next(1, 7);
+    }
+}
+
+public class TilfældighedsGeneratorMock : ITilfældighedsGenerator
+{
+    private int tal;
+    public TilfældighedsGeneratorMock(int tal)
+    {
+        this.tal = tal;
+    }
+    public int FindTalTilTerning()
+    {
+        return this.tal;
+    }
+}
+
+class Terning
+{
+    private ITilfældighedsGenerator generator;
+
+    public int Værdi { get; set; }
+
+    public Terning(ITilfældighedsGenerator rnd)
+    {
+        generator = rnd;
+        Ryst();
+    }
+
+    public void Ryst()
+    {
+        this.Værdi = generator.FindTalTilTerning();
+    }
+}
