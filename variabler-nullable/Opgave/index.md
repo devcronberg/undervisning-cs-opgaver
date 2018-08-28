b62b897098383f1725b22513cee859b8bb438a14
Michell Cronberg <admin@TA-8215-INS>
2018-08-28 17:31:02 +0200

    nullable

---
+++ variabler-nullable/Opgave/index.md
@@ -0,0 +1,12 @@
+﻿# Brug af metoder (nullable)
+
+Start en ny tom konsol applikation:
+
+* Tilføj en metode HentFraDb der tager en int som argument og return en nullable bool. Metoden simulerer kald til en database efter et felt med en mulig null-værdi
+  * Hvis int = 1 returneres true
+  * Hvis int = 2 restureres false
+  * Ellers returneres null
+
+* Test metoden i main ved at skrive ud til consol (null = '' i WriteLine)
+
+
---
+++ variabler-nullable/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variabler_nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool? res;
+            res = HentFraDb(1);
+            Console.WriteLine($"resultat = {res}");
+
+            res = HentFraDb(2);
+            Console.WriteLine($"resultat = {res}"); 
+
+            res = HentFraDb(3);
+            Console.WriteLine($"resultat = {res}");
+
+
+            // Hold console åben ved debug
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+        }
+
+
+        static bool? HentFraDb(int id)
+        {
+            if (id == 1)
+                return true;
+            if (id == 2)
+                return false;
+            return null;
+        }
+    }
+}
