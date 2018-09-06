5a236888f15354be68430bdd508bcab6ff00d5b3
Michell Cronberg <admin@TA-826-INS>
2018-09-06 12:41:51 +0200

    singleton

---
+++ klasser-singleton/Opgave/index.md
@@ -0,0 +1,17 @@
+﻿# Klasser (singleton)
+
+Du skal skabe en tom konsol-applikation med en enkelt klasse kaldet AppSettings. Den skal indeholde et enkelt offentlig felt (ved det godt - duer ikke - men ok lige her) kaldet int AppId. Men der må altid kun eksistere et AppSettings-objekt - så du skal:
+
+* have en privat constructor (der kan ikke skabes en instans)
+* have en privat statisk instans af AppSettings
+  * som kun kan returneres gennem en HentAppSettings-metode. Denne metode opretter en ny statisk instans hvis den ikke findes i forvejen.
+
+Så den skal kunne bruges som:
+
+```csharp
+AppSettings appSettings = AppSettings.HentAppSettings();
+appSettings.AppId = 10;
+Console.WriteLine(appSettings.AppId);   // 10
+appSettings = AppSettings.HentAppSettings();
+Console.WriteLine(appSettings.AppId);   // 10
+```
---
+++ klasser-singleton/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasser_singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppSettings appSettings = AppSettings.HentAppSettings();
+            appSettings.AppId = 10;
+            Console.WriteLine(appSettings.AppId);   // 10
+            appSettings = AppSettings.HentAppSettings();
+            Console.WriteLine(appSettings.AppId);   // 10
+        }
+    }
+
+    class AppSettings
+    {
+        public int AppId;
+        private static AppSettings o = null;
+
+        public static AppSettings HentAppSettings()
+        {
+            if (o == null)
+                o = new AppSettings();
+            return o;
+        }
+    }
+}
