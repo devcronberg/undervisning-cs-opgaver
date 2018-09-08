c4d7edebecc55a9a738d9442d79c18a88573b7aa
Michell Cronberg <michell@cronberg.dk>
2018-09-08 20:50:35 +0200

    xml

---
+++ linq2xml-simpel/Opgave/index.md
@@ -0,0 +1,52 @@
+﻿# LINQ to XML
+
+Du skal skabe en konsol app og i denne tilføje en xml-fil kaldet personer.xml med følgende indehold:
+
+```
+<?xml version="1.0" encoding="utf-8" ?>
+<personer>
+  <person id="1">
+    <navn>Mikkel</navn>
+    <alder>15</alder>
+  </person>
+  <person id="2">
+    <navn>Mathias</navn>
+    <alder>12</alder>
+  </person>
+  <person id="3">
+    <navn>Lene</navn>
+    <alder>53</alder>
+  </person>
+  <person id="4">
+    <navn>Michell</navn>
+    <alder>51</alder>
+  </person>
+  <person id="5">
+    <navn>Lis</navn>
+    <alder>73</alder>
+  </person>
+  <person id="6">
+    <navn>Bjarne</navn>
+    <alder>75</alder>
+  </person>
+  <person id="7">
+    <navn>Lise</navn>
+    <alder>41</alder>
+  </person>
+  <person id="8">
+    <navn>Peter</navn>
+  </person>
+</personer>
+```
+
+Sørg for, at filen er market som "Copy always" i "Copy to output directory" i egenskaberne til filen (Properties-vinduet).
+
+Du skal nu loade filen i et XDocument-objekt og skabe følgende metoder:
+
+- void VisAllePersoner(XDocument d)
+  - Viser alle navne på personer på konsolen
+- void VisAllePersonerSorteret(XDocument d, string sorterEfter)
+  - Viser alle navne på personer sorteret efter navn eller alder angivet som argument (se bort fra fejlhåndtering)
+- string FindNavn(XDocument d, int id)
+  - Finder et givet navn efter et id. Hvis intet findes skal der returneres "?"
+
---
+++ linq2xml-simpel/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace linq2xml_simpel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // forudsætter personer.xml i /bin
+            XDocument doc = XDocument.Load(@"personer.xml");
+            VisAllePersoner(doc);
+            Console.WriteLine();
+            VisAllePersonerSorteret(doc, "navn");
+            Console.WriteLine();
+            Console.WriteLine(FindNavn(doc, 1));
+
+            // Hold console åben ved debug
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+
+            void VisAllePersoner(XDocument d)
+            {
+                var elementer = d.Descendants("person");
+                foreach (var person in elementer)
+                    Console.WriteLine(person.Element("navn").Value);
+            }
+
+            void VisAllePersonerSorteret(XDocument d, string sorterEfter)
+            {
+                var elementer = d.Descendants("person").OrderBy(i => i.Element(sorterEfter).Value);
+                foreach (var person in elementer)
+                    Console.WriteLine(person.Element("navn").Value);
+            }
+
+            string FindNavn(XDocument d, int id)
+            {
+                var elementer = d.Descendants("person").Where(i => (int)i.Attribute("id") == id);
+                return elementer.FirstOrDefault()?.Element("navn").Value ?? "?";
+            }
+
+        }
+    }
+}
