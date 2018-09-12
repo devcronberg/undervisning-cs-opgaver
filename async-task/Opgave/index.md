4b85890afe409ca9334a49169066fc68a957ac25
Michell <admin@TA-826-INS>
2018-09-12 12:11:42 +0200

    .

---
+++ async-task/Opgave/index.md
@@ -0,0 +1,29 @@
+﻿# ASync (Fil)
+
+Du skal skabe en (7.3 - ret version i projektets properties/build/advanced) konsol app med følgende main:
+
+```csharp
+static async Task Main(string[] args) {}
+```
+
+og dan en tekstfil i c:\temp\ med noget tekst i. Brug evt denne metode:
+
+```csharp
+public static void DanFil(string fil)
+{
+    if (System.IO.File.Exists(fil))
+        System.IO.File.Delete(fil);
+
+    using (var s = System.IO.File.AppendText(fil))
+        for (int i = 0; i < 1000; i++)
+            s.WriteLine(Guid.NewGuid().ToString());
+}
+```
+
+Du skal nu skabe en asynkron metode HentTekst(string filnavn) der ved hjælp af en StreamReader (System.IO.File.OpenText) læser filen med og returnerer resultatet som en Task<string> svarende til indholdet af filen.
+
+
+
+<!-- footerstart -->
+> Denne opgave må ikke benyttes uden tilladelse fra Michell Cronberg (michell@cronberg.dk)
+<!-- footerslut -->
---
+++ async-task/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace async_task
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            DanFil(@"c:\temp\a1.txt");
+
+            //string t = HentTekst(@"c:\temp\a1.txt").Result;
+            string t = await HentTekst(@"c:\temp\a1.txt");
+            Console.WriteLine(t);
+        }
+
+        public static async Task<string> HentTekst(string fil)
+        {
+            using (var stream = System.IO.File.OpenText(fil))
+            {
+                return await stream.ReadToEndAsync();
+            }
+        }
+
+
+        public static void DanFil(string fil)
+        {
+            if (System.IO.File.Exists(fil))
+                System.IO.File.Delete(fil);
+
+            using (var s = System.IO.File.AppendText(fil))
+                for (int i = 0; i < 1000; i++)
+                    s.WriteLine(Guid.NewGuid().ToString());
+        }
+    }
+}
