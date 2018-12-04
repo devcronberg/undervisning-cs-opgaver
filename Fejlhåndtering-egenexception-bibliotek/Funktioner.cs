bc458020dfab2d36a2a490dfa37d254f64b2e0f2
admin <admin@TA-826-INS>
2018-12-04 16:08:58 +0100

    .

---
+++ Fejlhåndtering-egenexception-bibliotek/Funktioner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fejlhåndtering_egenexception_bibliotek
+{
+    public static class Funktioner
+    {
+        public static void OpdaterTal(string sti) {
+
+            
+            if (!System.IO.File.Exists(sti))
+                throw new ApplicationException("Filen " + sti + " kan ikke findes");
+
+            string indhold = "";
+            try
+            {
+                indhold = System.IO.File.ReadAllText(sti);
+                int tal = Convert.ToInt32(indhold);
+                tal++;
+                System.IO.File.WriteAllText(sti, tal.ToString());
+                Console.WriteLine($"Tal i fil er nu {tal}");
+            }
+            catch (System.FormatException)
+            {
+                throw new ApplicationException($"'{indhold}' kan ikke konverteres til et heltal");
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+}
