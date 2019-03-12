75494265a79dd96b84a09379ce9386df3deb8be8
Michell <admin@AR-114-INS>
2019-03-12 10:43:17 +0100

    Bæger

---
+++ array-yatzybaeger/Opgave/index.md
@@ -0,0 +1,84 @@
+﻿# Bæger med terninger
+
+Opret en ny tom konsol-applikation og definer en terning som (eller skab din egen):
+
+```csharp
+public class Terning
+{        
+    private static System.Random rnd = new Random();
+    private int _værdi;
+             
+    public int Værdi
+    {
+        get
+        {
+                
+            return this._værdi;
+        }
+
+        set
+        {
+            if (value < 1 || value > 6)
+                throw new ApplicationException("Forkert værdi på terning");
+                
+            this._værdi = value;
+        }
+    }
+
+        
+    public Terning()
+    {
+        this.Ryst();
+    }
+
+    public Terning(int værdi)
+    {
+        this.Værdi = værdi;
+    }
+
+
+        
+    public void Ryst()
+    {
+        this.Værdi = rnd.Next(1, 7);
+    }
+
+    public void Skriv()
+    {
+        Console.WriteLine(this.ToString());
+    }
+
+    public override string ToString()
+    {
+        return $"[{this.Værdi}]";
+    }
+}
+```
+Din opgave er nu at skabe en klasse Bæger som kan indeholde fem terninger i et private 
+array af Terning. Klassen skal indeholde følgende medlemmer:
+
+- en constructor der tilføje fem nye terninger til det interne arrays når der skabes en instans af Bæger
+- en Ryst-metode der ryster alle terninger i bæger
+- en Skriv der udskriver alle terninger som [x] [x] [x] [x] [x] 
+
+Klassen skal kunne bruges som:
+
+```csharp
+Bæger b = new Bæger();
+b.Skriv();
+b.Ryst();
+b.Skriv();
+```
+
+hvilket kunne udskrive:
+
+```
+[4][5][5][2][1]
+[4][3][6][5][5]
+```
+
+Se evt. [løsning](https://github.com/devcronberg/undervisning-cs-opgaver/blob/master/arrays-baeger/Program.cs)
+
+<!-- footerstart -->
+> Denne opgave må ikke benyttes uden tilladelse fra Michell Cronberg (michell@cronberg.dk)
+<!-- footerslut -->
---
+++ array-yatzybaeger/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays_yatzybaeger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Bæger b = new Bæger();
+b.Skriv();
+b.Ryst();
+b.Skriv();
+        }
+    }
+
+    public class Bæger
+    {
+        private Terning[] terninger = new Terning[5];
+
+        public Bæger()
+        {
+            for (int i = 0; i < 5; i++)
+                terninger[i] = new Terning();
+
+        }
+
+        public void Ryst()
+        {
+            for (int i = 0; i < 5; i++)
+                terninger[i].Ryst();
+        }
+
+        public void Skriv()
+        {
+            for (int i = 0; i < 5; i++)
+                Console.Write(terninger[i].ToString());
+            Console.WriteLine();
+        }
+    }
+
+
+    public class Terning
+    {
+        private static System.Random rnd = new Random();
+        private int _værdi;
+
+        public int Værdi
+        {
+            get
+            {
+
+                return this._værdi;
+            }
+
+            set
+            {
+                if (value < 1 || value > 6)
+                    throw new ApplicationException("Forkert værdi på terning");
+
+                this._værdi = value;
+            }
+        }
+
+
+        public Terning()
+        {
+            this.Ryst();
+        }
+
+        public Terning(int værdi)
+        {
+            this.Værdi = værdi;
+        }
+
+
+
+        public void Ryst()
+        {
+            this.Værdi = rnd.Next(1, 7);
+        }
+
+        public void Skriv()
+        {
+            Console.WriteLine(this.ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"[{this.Værdi}]";
+        }
+    }
+}
