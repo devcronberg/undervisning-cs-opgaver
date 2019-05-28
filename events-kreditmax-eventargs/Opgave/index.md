876ef33c17f6c3f3d76d6c10958dbafa90594edd
admin <admin@TA-826-INS>
2019-05-28 14:22:39 +0200

    eventargs

---
+++ events-kreditmax-eventargs/Opgave/index.md
@@ -0,0 +1,36 @@
+﻿# Events (KreditMax)
+
+Du skal skabe en konsol app med en klasse Kunde som følger:
+
+```csharp
+public class Kunde
+{
+    // event mangler
+    public string Navn { get; set; }
+    public int Saldo { get; set; }
+    public int KreditMax { get; set; }
+    public void Køb(int værdi) {
+        Console.WriteLine("Kunde {0} køber for {1}", Navn, værdi);
+        this.Saldo -= værdi;
+        if (Saldo < KreditMax) {
+	// kald til event mangler
+        }
+    }
+}
+```
+
+I klassen skal der være en hændelse (event) kaldet KreditOverskredet. Den skal kaldes såfremt kreditmax overskrides. Hændelsen skal være af typen EventArgs<KreditOverskredetEventArgs>, der som argument tager en sender (object) og KreditOverskredetEventArgs (din egen type som består af en Saldo). Se evt. [løsning](https://github.com/devcronberg/undervisning-cs-opgaver/blob/master/events-kreditmax/Program.cs).
+
+Følgende kode kan benyttes som test:
+
+```csharp
+Kunde k = new Kunde() { KreditMax = -500 };
+k.KreditOverskredet += (s, e) => {
+    Console.WriteLine($"Kredit overskredet - saldo = {e.Saldo:N2}");
+};
+k.Køb(100);
+k.Køb(600);	// Her skal metoden bundet til KreditOverskredet blive kaldt automatisk
+```
+<!-- footerstart -->
+> Denne opgave må ikke benyttes uden tilladelse fra Michell Cronberg (michell@cronberg.dk)
+<!-- footerslut -->
---
+++ events-kreditmax-eventargs/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace events_kreditmax_eventargs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kunde k = new Kunde() { KreditMax = -500 };
+            k.KreditOverskredet += (s, e) =>
+            {                
+                Console.WriteLine($"Kredit overskredet - saldo = {e.Saldo:N2}");
+            };
+            k.Køb(100);
+            k.Køb(600);
+
+            // Hold console åben ved debug
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                Console.Write("Press any key to continue . . . ");
+                Console.ReadKey();
+            }
+        }
+    }
+
+
+    public class Kunde
+    {
+        public event EventHandler<KreditOverskredetEventArgs> KreditOverskredet;
+        public string Navn { get; set; }
+        public int Saldo { get; set; }
+        public int KreditMax { get; set; }
+        public void Køb(int værdi)
+        {
+            Console.WriteLine($"Kunde {this.Navn} køber for {værdi}");
+            this.Saldo -= værdi;
+            if (Saldo < KreditMax)
+            {
+                if (KreditOverskredet != null)
+                    KreditOverskredet(this, new KreditOverskredetEventArgs(this.Saldo));
+            }
+        }
+    }
+
+    public class KreditOverskredetEventArgs : EventArgs
+    {
+
+        public int Saldo { get; set; }
+
+        public KreditOverskredetEventArgs(int saldo)
+        {
+            this.Saldo = saldo;
+        }
+
+    }
+}
