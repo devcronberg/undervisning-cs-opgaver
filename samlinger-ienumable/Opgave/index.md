ec9ffbae8d8d340822dc1b75b25cfbc083bb169c
Michell Cronberg <michell@cronberg.dk>
2019-06-15 11:34:05 +0200

    IEnumerable

---
+++ samlinger-ienumable/Opgave/index.md
@@ -0,0 +1,60 @@
+﻿# Samlinger (IEnumerable)
+
+Du skal skabe en tom konsol app med følgende klasser:
+
+## Bil
+
+Klassen skal have egenskaberne Model (string), Mærke (string), Årgang (int) og Nummerplade (string). Samlinger af 
+klassen skal kunne sorteres efter årgang, så du skal implementere IComparable<Bil>. Sørg for at overskrive ToString så det er nemt at 
+skrive instanser ud på konsollen. Brug evt JSON.NET fra NuGet, og metoden Newtonsoft.Json.JsonConvert.SerializeObject() - så er det nemt
+at se alle egenskaber.
+
+## Garage
+
+Klassen skal have en privat List<Bil>, samt metoderne BilInd(Bil) og Bil BilUd(string) som hhv skal tilføje og fjerne (efter nummerplade) en bil. Du skal kunne ForEach'e 
+over biler i garagen så du skal implementere IEnumerable<Bil>.
+
+
+Prøv det eventuelt med følgende kode i Main:
+
+```csharp
+Garage g = new Garage();
+g.BilInd(new Bil() { Model = "a", Mærke = "x", NummerPlade = "xx", Årgang = 2000 });
+g.BilInd(new Bil() { Model = "b", Mærke = "y", NummerPlade = "yy", Årgang = 2019 });
+g.BilInd(new Bil() { Model = "c", Mærke = "z", NummerPlade = "ww", Årgang = 1978 });
+foreach (var item in g)
+    Console.WriteLine(item);
+g.Sorter();
+foreach (var item in g)
+    Console.WriteLine(item);
+Console.WriteLine();
+var b = g.BilUd("ww");
+Console.WriteLine(b);
+Console.WriteLine();
+foreach (var item in g)
+    Console.WriteLine(item);
+
+// Test af BilUd ved fejl
+```
+
+Resultat:
+
+```
+{"Mærke":"x","Model":"a","Årgang":2000,"NummerPlade":"xx"}
+{"Mærke":"y","Model":"b","Årgang":2019,"NummerPlade":"yy"}
+{"Mærke":"z","Model":"c","Årgang":1978,"NummerPlade":"ww"}
+{"Mærke":"z","Model":"c","Årgang":1978,"NummerPlade":"ww"}
+{"Mærke":"x","Model":"a","Årgang":2000,"NummerPlade":"xx"}
+{"Mærke":"y","Model":"b","Årgang":2019,"NummerPlade":"yy"}
+
+{"Mærke":"z","Model":"c","Årgang":1978,"NummerPlade":"ww"}
+
+{"Mærke":"x","Model":"a","Årgang":2000,"NummerPlade":"xx"}
+{"Mærke":"y","Model":"b","Årgang":2019,"NummerPlade":"yy"}
+```
+
+Se [løsning](https://github.com/devcronberg/undervisning-cs-opgaver/blob/master/samlinger-ienumable/Program.cs)
+
+<!-- footerstart -->
+> Denne opgave må ikke benyttes uden tilladelse fra Michell Cronberg (michell@cronberg.dk)
+<!-- footerslut -->
---
+++ samlinger-ienumable/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace samlinger_ienumable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Garage g = new Garage();
+g.BilInd(new Bil() { Model = "a", Mærke = "x", NummerPlade = "xx", Årgang = 2000 });
+g.BilInd(new Bil() { Model = "b", Mærke = "y", NummerPlade = "yy", Årgang = 2019 });
+g.BilInd(new Bil() { Model = "c", Mærke = "z", NummerPlade = "ww", Årgang = 1978 });
+foreach (var item in g)
+    Console.WriteLine(item);
+g.Sorter();
+foreach (var item in g)
+    Console.WriteLine(item);
+Console.WriteLine();
+var b = g.BilUd("ww");
+Console.WriteLine(b);
+Console.WriteLine();
+foreach (var item in g)
+    Console.WriteLine(item);
+
+// Test af BilUd ved fejl
+            // b = g.BilUd("wa");
+        }
+    }
+
+    class Bil : IComparable<Bil>
+    {
+        public string Mærke { get; set; }
+        public string Model { get; set; }
+        public int Årgang { get; set; }
+        public string NummerPlade { get; set; }
+
+        public int CompareTo(Bil other)
+        {
+            return this.Årgang.CompareTo(other.Årgang);
+        }
+
+        public override string ToString()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+        }
+    }
+
+    class Garage : IEnumerable<Bil>
+    {
+        private List<Bil> _biler;
+
+        public Garage()
+        {
+            _biler = new List<Bil>();
+        }
+
+        public IEnumerator<Bil> GetEnumerator()
+        {
+            return ((IEnumerable<Bil>)_biler).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<Bil>)_biler).GetEnumerator();
+        }
+
+        public void BilInd(Bil b)
+        {
+            _biler.Add(b);
+        }
+
+        public Bil BilUd(string nummerPlade)
+        {
+            Bil b = _biler.Find(i => i.NummerPlade == nummerPlade);
+            if (b == null)
+                throw new ApplicationException("Bil findes ikke");
+            _biler.Remove(b);
+            return b;
+        }
+
+        public void Sorter()
+        {
+            _biler.Sort();
+        }
+
+    }
+}
