1451105719248f4d5e0a968b02b1d7409ac17078
Michell Cronberg <michell@cronberg.dk>
2019-06-23 15:20:24 +0200

    avanceretlinq-extensions

---
+++ avanceretlinq-extensions/Opgave/readme.md
@@ -0,0 +1,35 @@
+﻿# LINQ med egne extensions
+
+Du skal oprette en tom konsol applikation og skabe to udvidelser til LINQ to objects
+
+- Tilføj en offentlig statisk klasse
+- Tilføj en extension metode kaldet HverAnden\<T\>, som returnerer hvert andet element i en liste
+- Overload metoden Where\<T\> med en version, der udover en predicate og modtager en int der angiver hvor mange elementer der skal returneres
+
+De skal virke som:
+
+```csharp
+List<int> lst1 = new List<int>() { 5, 4, 2, 3, 7, 10, 2 };
+foreach (var item in lst1.HverAnden())
+    Console.WriteLine(item);
+Console.WriteLine();
+foreach (var item in lst1.Where(i => i < 5, 2))
+    Console.WriteLine(item);
+```
+som burde resultere i:
+```
+5
+2
+7
+2
+
+4
+2
+```
+
+Se eventuelt [min løsning](https://github.com/devcronberg/undervisning-cs-opgaver/blob/master/avanceretlinq-extensions/Program.cs).
+
+
+<!-- footerstart -->
+> Denne opgave må ikke benyttes uden tilladelse fra Michell Cronberg (michell@cronberg.dk)
+<!-- footerslut -->
---
+++ avanceretlinq-extensions/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace avanceretlinq_extensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lst1 = new List<int>() { 5, 4, 2, 3, 7, 10, 2 };
+            foreach (var item in lst1.HverAnden())
+                Console.WriteLine(item);
+            Console.WriteLine();
+            foreach (var item in lst1.Where(i => i < 5, 2))
+                Console.WriteLine(item);
+
+        }
+    }
+
+    public static class MineExtensions
+    {
+        public static IEnumerable<T> HverAnden<T>(this IEnumerable<T> source)
+        {
+            List<T> list = new List<T>();
+
+            int i = 0;
+
+            foreach (var element in source)
+            {
+                if (i % 2 == 0)
+                {
+                    list.Add(element);
+                }
+
+                i++;
+            }
+
+            return list;
+        }
+
+        public static IEnumerable<T> Where<T>(this IEnumerable<T> source, Func<T, bool> where, int take)
+        {
+            return source.Where(where).Take(take);
+        }
+    }
+}
