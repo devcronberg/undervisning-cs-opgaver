4bb63e00e1c93accfcc928bf4c25e3503939892c
Michell Cronberg <admin@AR-114-INS>
2019-07-01 13:57:30 +0200

    lambda

---
+++ delegates-lambda-beregner/Opgave/index.md
@@ -0,0 +1,31 @@
+﻿# Delegates (beregner med lambda)
+
+Du skal skabe en tom konsol med en Beregner-metode der tager to heltal til beregning samt 
+en Func (retunerer en int og modtager to int's), der henviser der den funktion der ønskes benyttes. 
+
+Kald kan se således ud:
+
+```csharp
+int res = 0;
+res = Beregner(1, 1, (a,b)=> a + b);
+Console.WriteLine(res);
+```
+
+Prøv at kalde Beregner-funktionen så den både trækker fra, ganger og dividerer.
+
+Prøv også at skabe en metode FindTilfældigFunktion, der returnerer
+en Func (delegate) med en af de fire metoder (plus, minus, gange eller divider)
+afhængig af det konkrete millisekund (under 250=plus, 250-500=minus osv). kald
+til funktionen kan se således ud:
+
+```csharp
+var f = FindTilfældigFunktion();
+res = f(5, 5);
+Console.WriteLine(res);
+```
+
+Se evt. [løsning](https://github.com/devcronberg/undervisning-cs-opgaver/blob/master/delegates-lambda-beregner/Program.cs)
+
+<!-- footerstart -->
+> Denne opgave må ikke benyttes uden tilladelse fra Michell Cronberg (michell@cronberg.dk)
+<!-- footerslut -->
---
+++ delegates-lambda-beregner/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates_lambda_beregner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int res = 0;
+            res = Beregner(1, 1, (a, b) => a + b);
+            Console.WriteLine(res);
+            res = Beregner(1, 1, (a, b) => a - b);
+            Console.WriteLine(res);
+
+            var f = FindTilfældigFunktion();
+            res = f(5, 5);
+            Console.WriteLine(res);
+
+        }
+
+        public static int Beregner(int a, int b, Func<int, int, int> funktion)
+        {
+            return funktion(a, b);
+        }
+
+        public static Func<int, int, int> FindTilfældigFunktion()
+        {
+            if (System.DateTime.Now.Millisecond <= 250)
+                return (a, b) => a + b;
+            if (System.DateTime.Now.Millisecond <= 500)
+                return (a, b) => a - b;
+            if (System.DateTime.Now.Millisecond <= 750)
+                return (a, b) => a * b;
+
+            return (a, b) => a / b;
+
+        }
+    }
+}
