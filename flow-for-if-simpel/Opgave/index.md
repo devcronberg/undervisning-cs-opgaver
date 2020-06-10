38f97f8bdef0cfbf78ef7ab28a6d4ca7b2154ba9
Michell <admin@TA-821-INS>
2020-06-10 12:47:34 +0200

    flow

---
+++ flow-for-if-simpel/Opgave/index.md
@@ -0,0 +1,13 @@
+﻿# Simpelt flow (for og if)
+
+I en tom konsol applikation skal du
+
+- Tælle fra 0 til 9 og udskrive tal
+- Tælle fra 1 til 20 men kun hver anden (1-3-5-7...). Udskriv tal
+- Tælle fra 1 til 20 og udskriv tal - men lige tal skal være gule og ulige tal røde
+
+Se evt. [løsning for inspiration](https://github.com/devcronberg/undervisning-cs-opgaver/blob/master/flow-for-if-simpel/Program.cs).
+
+<!-- footerstart -->
+> Denne opgave må ikke benyttes uden tilladelse fra Michell Cronberg (michell@cronberg.dk)
+<!-- footerslut -->
---
+++ flow-for-if-simpel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace flow_for_if_simpel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tæl fra 0 til 9
+            for (int i = 0; i < 9; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine();
+
+            // Tæl fra 1 - 20 men kun hver anden (1-3-5-7...)
+            for (int i = 1; i < 21; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Tæl fra 1 - 20. Lige tal skal være gule og ulige tal røde
+            for (int i = 1; i < 21; i++)
+            {
+                if (i % 2 == 0)
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                else
+                    Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
