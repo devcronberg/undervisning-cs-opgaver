911d1a8f45ab67df3e2a4a44711243ca14e68551
Michell Cronberg <michell@cronberg.dk>
2020-11-29 10:32:06 +0100

    io

---
+++ io-streaming/Opgave/readme.md
@@ -0,0 +1,9 @@
+﻿# Brug af StreamWriter
+
+I denne opgave skal du skrive 100.000 Guids (```System.Guid.NewGuid().ToString()```) til en fil på disken. 
+
+Du kunne vælge først at danne en string og gemme denne men ved mange poster bliver det hurtigt for tungt.
+Så i denne opgave skal du åbne en ```System.IO.StreamWriter``` med ```System.IO.File.OpenWrite(sti)``` og benytte
+instansen af StreamWriter's WriteLine-metode.
+
+Se min løsning [her](https://github.com/devcronberg/undervisning-cs-opgaver/blob/master/io-streaming/Program.cs).
---
+++ io-streaming/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace io_streaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sti = "c:\\temp\\data.txt";
+            GemGuids(sti);
+        
+        }
+
+        static void GemGuids(string sti) {
+            if (System.IO.File.Exists(sti))
+                System.IO.File.Delete(sti);
+            using StreamWriter w = new StreamWriter(System.IO.File.OpenWrite(sti), System.Text.Encoding.UTF8);
+            for (int i = 0; i < 100000; i++)
+                w.WriteLine(Guid.NewGuid().ToString());
+            w.Close();
+        }
+    }
+}
