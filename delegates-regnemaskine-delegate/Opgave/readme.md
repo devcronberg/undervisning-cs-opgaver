87d1c220f7f3cfed4d2ecf2aaa7a8dc407dfef74
Michell <Admin@TA-826-INS>
2020-11-30 13:05:58 +0100

    regnemaskine

---
+++ delegates-regnemaskine-delegate/Opgave/readme.md
@@ -0,0 +1,10 @@
+﻿# Regnemaskine (rå delegates)
+
+I denne opgave skal du tilføje funktioner til en regnemaskine som
+jeg har lavet på forhånd. Start med at skabe en tom konsol applikation
+og kopier følgende kode 
+
+
+ind i projektet - enten i bunden af Program.cs eller
+som en fil for sig selv. Du behøver ikke forstå koden - du skal bare bruge den.
+
---
+++ delegates-regnemaskine-delegate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace delegates_regnemaskine_delegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegnemaskineApp.RegnemaskineConsoleUI r =
+                new RegnemaskineApp.RegnemaskineConsoleUI();
+            // Nyt delegate objekt med lang syntaks
+            r.TilføjBeregning("minus", new RegnemaskineApp.BeregnDelegate(Minus));
+            // Nyt delegate objekt med kort syntaks
+            r.TilføjBeregning("gange", Gange);
+            // Nyt delegate objekt gemmes i variabel
+            RegnemaskineApp.BeregnDelegate f = new RegnemaskineApp.BeregnDelegate(Divider);
+            r.TilføjBeregning("divider", f);
+            // Metode fra System.Math
+            r.TilføjBeregning("max", Math.Max);
+
+            r.Start();
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Gange(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divider(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
+
