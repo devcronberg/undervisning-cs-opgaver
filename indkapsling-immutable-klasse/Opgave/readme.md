c33c26b993ee48f9d0d85e14a7c3f2945b464234
Michell Cronberg <michell@cronberg.dk>
2020-12-02 13:50:18 +0100

    immutable

---
+++ indkapsling-immutable-klasse/Opgave/readme.md
@@ -0,0 +1,49 @@
+﻿# En uforanderlig (immutable) klasse
+
+I denne opgave skal du designe en immutable (uforanderlig) klasse Person med to egenskaber
+
+PersonId (int)
+Navn (string)
+
+Til at starte med skal du designe klassen således, at egenskaberne udelukkende skal kunne 
+tildeles en værdi gennem en brugerdefineret konstruktør (ingen konstruktør uden argumenter - man skal benytte den
+brugerdefinerede konstruktør). 
+
+Når først et objekt er skabt må data ikke kunne ændres.
+
+Klassen skal også bestå af en overskrivning af ToString således, at den returnerer eksempelvis
+
+```csharp
+Console.WriteLine(o);	// Person { PersonId: 1, Navn: Mikkel } 
+```
+
+Med klassen skal du kunne benytte følgende kode
+
+```csharp
+Person p1 = new Person(1, "Mikkel");
+Console.WriteLine(p1.Navn);
+// p1.Navn = "Mathias";     // Fejl
+Console.WriteLine(p1);      // Person { PersonId: 1, Navn: Mikkel }
+
+// Hvis du ønsker en kopi med ændrede data må du
+Person p2 = new Person(2, p1.Navn);
+Console.WriteLine(p2);      // Person { PersonId: 2, Navn: Mikkel }
+```
+
+Prøv at skabe to objekter med samme data og sammenlign:
+
+```csharp
+Person p3 = new Person(1, "Mathias");
+Person p4 = new Person(1, "Mathias");
+Console.WriteLine(p3 == p4);// false
+```
+
+Sammenligning returnerer true fordi variablerne ikke indeholder samme reference - men det skal du ændre på nu ved at 
+overskrive metoderne Equals og GetHashCode samt operatorerne == og !=. Du behøver ikke skrive koden selv med lade VS gøre dette
+ved at vælge "Override "
+
+![](override.png)
+
+Så før at alle egenskaber er valgt og at der implementeres IEquatable
+
+![](override1.png)
---
+++ indkapsling-immutable-klasse/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace indkapsling_immutable_klasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Person p1 = new Person(1, "Mikkel");
+            Console.WriteLine(p1.Navn);
+            // p1.Navn = "Mathias";     // Fejl
+            Console.WriteLine(p1);      // Person { PersonId: 1, Navn: Mikkel }
+
+            // Hvis du ønsker en kopi med ændrede data må du
+            Person p2 = new Person(2, p1.Navn);
+            Console.WriteLine(p2);      // Person { PersonId: 2, Navn: Mikkel }
+
+
+Person p3 = new Person(1, "Mathias");
+Person p4 = new Person(1, "Mathias");
+Console.WriteLine(p3 == p4);// false
+
+        }
+    }
+
+    class Person : IEquatable<Person>
+    {
+        public int PersonId { get; private set; }
+        public string Navn { get; private set; }
+
+        public Person(int personId, string navn)
+        {
+            this.PersonId = personId;
+            this.Navn = navn;
+        }
+
+        public override string ToString()
+        {
+            return $"Person {{ PersonId: { this.PersonId }, Navn: { this.Navn } }}";
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Person);
+        }
+
+        public bool Equals(Person other)
+        {
+            return other != null &&
+                   PersonId == other.PersonId &&
+                   Navn == other.Navn;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(PersonId, Navn);
+        }
+
+        public static bool operator ==(Person left, Person right)
+        {
+            return EqualityComparer<Person>.Default.Equals(left, right);
+        }
+
+        public static bool operator !=(Person left, Person right)
+        {
+            return !(left == right);
+        }
+    }
+}
