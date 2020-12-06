4d487f2541553e93aa35751eff69159e440838c5
Michell Cronberg <michell@cronberg.dk>
2020-12-06 18:18:39 +0100

    .

---
+++ ConsoleOpgave-Configuration/Opgave/readme.md
@@ -0,0 +1,70 @@
+﻿# Brug af konfigurationsfiler
+
+I denne opgave skal du arbejde videre på denne opgave
+
+https://github.com/devcronberg/undervisning-cs-opgaver/tree/master/ConsoleOpgave-Parser/Opgave#brug-af-commandlineparser
+
+hvor du skal skabe en applikation som kopierer en fil til en mappe. I den opgave skal både fil og mappe
+angives som parametre, men i denne opgave skal brugeren angive den mappe der skal kopieres til i
+en konfigurationsfil kaldet appsettings.json:
+
+```json
+{
+    "Directory": "c:\\temp"
+}
+```
+
+Du kan starte med at kopiere min (eller din egen) løsning fra ovennævnte opgave ind som skabelon (husk pakken 
+CommandLineParser), og så tilrette applikationen således, at ovennævnte appsetting-fil benyttes i stedet til "directory". Denne
+parameter skal således fjernes så app kan startes med
+
+```
+app.exe -f c:\temp\data.txt
+app.exe -f c:\temp\data.txt -r
+```
+
+For at arbejde med appsettings skal du tilføje følgende pakker
+
+```
+Microsoft.Extensions.Configuration.Json
+Microsoft.Extensions.Configuration.Binder
+```
+
+og du kan blive lidt inspireret af brug af appsettings i følgende kode:
+
+```csharp
+using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleOpgave_Configuration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = GetConfiguration();
+            Console.WriteLine(configuration.A);
+            Console.WriteLine(configuration.B);
+            Console.WriteLine(configuration.C);
+        }
+
+        static Configuration GetConfiguration() {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            var c = new Configuration();            
+            configuration.Bind(c);
+            return c;
+        }
+    }
+
+    class Configuration {
+        public string A { get; set; }
+        public int B { get; set; }
+        public bool C { get; set; }
+    }
+}
+```
+Se min løsning [her](https://github.com/devcronberg/undervisning-cs-opgaver/blob/master/ConsoleOpgave-Configuration/Program.cs)
---
+++ ConsoleOpgave-Configuration/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using CommandLine;
+
+namespace ConsoleOpgave_Configuration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(Run);
+
+        }
+
+        static Configuration GetConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+            var c = new Configuration();
+            configuration.Bind(c);
+            return c;
+        }
+
+        private static void Run(Options o)
+        {
+            try
+            {
+                var configuration = GetConfiguration();
+
+                if (!System.IO.File.Exists(o.File))
+                {
+                    Console.WriteLine(o.File + " does not exists");
+                    return;
+                }
+
+                if (!System.IO.Directory.Exists(configuration.Directory))
+                {
+                    Console.WriteLine(configuration.Directory + " does not exists");
+                    return;
+                }
+
+                string file = Path.GetFileName(o.File);
+                File.Copy(o.File, Path.Combine(configuration.Directory, file), true);
+                if (o.RemoveFile)
+                {
+                    File.Delete(o.File);
+                }
+                Console.WriteLine($"{o.File} is copied to {configuration.Directory}" + (o.RemoveFile ? " and removed" : ""));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+
+    class Options
+    {
+        [Option('r', "remove", Required = false, HelpText = "Remove file after copy")]
+        public bool RemoveFile { get; set; }
+
+        [Option('f', "file", Required = true, HelpText = "File to copy")]
+        public string File { get; set; }
+    }
+
+    class Configuration
+    {
+        public string Directory { get; set; }
+    }
+}
+
