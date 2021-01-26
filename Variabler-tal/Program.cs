6ac5add48c54527b6808f931539a42e32d21f309
Michell Cronberg <mcronberg@users.noreply.github.com>
2021-01-26 08:55:55 +0100

    Update Program.cs

--- Variabler-tal/Program.cs
+++ Variabler-tal/Program.cs
@@ -10,21 +10,13 @@ namespace MinApp
             heltal++;
             heltal--;
             heltal += 20;
-            Console.WriteLine($"Heltal = {heltal}");
+            Console.WriteLine(heltal);
 
             double kommatal = 12.5;
             kommatal++;
             kommatal--;
             kommatal *= 2;
-            Console.WriteLine($"Kommatal = {kommatal}");
-
-
-            // Hold console åben ved debug (kun nødvendigt ved .NET Framework - ikke .NET Core)
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                Console.Write("Press any key to continue . . . ");
-                Console.ReadKey();
-            }
+            Console.WriteLine(kommatal);
 
         }
     }
