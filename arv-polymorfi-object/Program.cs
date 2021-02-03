e7af497e83d6dc589176f335a1903ccb1c9f4af5
Michell Cronberg <mcronberg@users.noreply.github.com>
2021-02-03 11:12:50 +0100

    Update Program.cs

--- arv-polymorfi-object/Program.cs
+++ arv-polymorfi-object/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json; // Kun til JSON (ekstra opgaven)
 
 namespace arv_polymorfi_object
 {
