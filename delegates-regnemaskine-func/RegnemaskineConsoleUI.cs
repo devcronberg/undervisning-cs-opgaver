69d9b054c78fcf6cb463c2801c4d0384cfc012fe
devcronberg <devcronberg@gmail.com>
2022-09-12 11:08:36 +0200

    Update RegnemaskineConsoleUI.cs

--- delegates-regnemaskine-func/RegnemaskineConsoleUI.cs
+++ delegates-regnemaskine-func/RegnemaskineConsoleUI.cs
@@ -1,71 +1,107 @@
-﻿namespace RegnemaskineApp
+using System;
+
+namespace delegates_regnemaskine_func
 {
-    using System;
-    using System.Collections.Generic;
-    
-    public class RegnemaskineConsoleUI
+    class Program
     {
-        private Dictionary<string, Func<int, int, int>> beregninger { get; set; }
-
-        public RegnemaskineConsoleUI()
+        static void Main(string[] args)
         {
-            beregninger = new Dictionary<string, Func<int, int, int>>();
-            TilføjBeregning("plus", (a, b) => a + b);
+            RegnemaskineApp.RegnemaskineConsoleUI r =
+                new RegnemaskineApp.RegnemaskineConsoleUI();
+
+            Func<int, int, int> f1 = Minus;
+            r.TilføjBeregning("minus", f1);
+            // Eller blot
+            // r.TilføjBeregning("minus", Minus);
+
+            r.TilføjBeregning("gange", Gange);
+            r.TilføjBeregning("divider", Divider);
+            // Metode fra System.Math
+            r.TilføjBeregning("max", Math.Max);
+
+            r.Start();
         }
 
-        public void TilføjBeregning(string navn, Func<int, int, int> funktion)
+        public static int Minus(int a, int b)
         {
-            beregninger.Add(navn.ToLower(), funktion);
+            return a - b;
         }
-
-        public void Start()
+        public static int Gange(int a, int b)
         {
-            try
+            return a * b;
+        }
+        public static int Divider(int a, int b)
+        {
+            return a / b;
+        }
+    }
+
+    namespace RegnemaskineApp
+    {
+        public class RegnemaskineConsoleUI
+        {
+            private Dictionary<string, Func<int, int, int>> beregninger { get; set; }
+
+            public RegnemaskineConsoleUI()
             {
-                do
-                {
-                    Console.Clear();
-                    Console.BackgroundColor = ConsoleColor.White;
-                    Console.ForegroundColor = ConsoleColor.Black;
-                    Console.WriteLine("REGNEMASKINE");
-                    Console.BackgroundColor = ConsoleColor.Black;
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.WriteLine("------------");
-                    Console.WriteLine();
-                    Console.WriteLine("Skriv navnet på en af følgende beregninger:");
-                    foreach (var item in beregninger)
-                    {
-                        Console.WriteLine(" - " + item.Key);
-                    }
-                    Console.WriteLine();
-                    string b = Console.ReadLine().ToLower();
-                    if (!beregninger.ContainsKey(b))
-                    {
-                        Console.WriteLine("Beregning findes ikke");
-                        return;
-                    }
-                    var beregning = beregninger[b];
-                    Console.WriteLine("Indtast tal1:");
-                    string tal1s = Console.ReadLine();
-                    Console.WriteLine("Indtast tal2:");
-                    string tal2s = Console.ReadLine();
-                    int tal1 = Convert.ToInt32(tal1s);
-                    int tal2 = Convert.ToInt32(tal2s);
-                    int res = beregning(tal1, tal2);
-                    Console.WriteLine("Resultatet er:");
-                    Console.WriteLine(res);
-                    Console.WriteLine("Tryk Esc for at afbryde eller en anden tast for at foretage ny beregning");
-                    var tast = Console.ReadKey();
-                    if (tast.Key == ConsoleKey.Escape)
-                        break;
-
-                } while (true);
-
+                beregninger = new Dictionary<string, Func<int, int, int>>();
+                TilføjBeregning("plus", (a, b) => a + b);
             }
-            catch (Exception ex)
+
+            public void TilføjBeregning(string navn, Func<int, int, int> funktion)
             {
-                Console.WriteLine("Hov - der er sket en fejl (" + ex.Message + ")");
-                return;
+                beregninger.Add(navn.ToLower(), funktion);
+            }
+
+            public void Start()
+            {
+                try
+                {
+                    do
+                    {
+                        Console.Clear();
+                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine("REGNEMASKINE");
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Gray;
+                        Console.WriteLine("------------");
+                        Console.WriteLine();
+                        Console.WriteLine("Skriv navnet på en af følgende beregninger:");
+                        foreach (var item in beregninger)
+                        {
+                            Console.WriteLine(" - " + item.Key);
+                        }
+                        Console.WriteLine();
+                        string b = Console.ReadLine().ToLower();
+                        if (!beregninger.ContainsKey(b))
+                        {
+                            Console.WriteLine("Beregning findes ikke");
+                            return;
+                        }
+                        var beregning = beregninger[b];
+                        Console.WriteLine("Indtast tal1:");
+                        string tal1s = Console.ReadLine();
+                        Console.WriteLine("Indtast tal2:");
+                        string tal2s = Console.ReadLine();
+                        int tal1 = Convert.ToInt32(tal1s);
+                        int tal2 = Convert.ToInt32(tal2s);
+                        int res = beregning(tal1, tal2);
+                        Console.WriteLine("Resultatet er:");
+                        Console.WriteLine(res);
+                        Console.WriteLine("Tryk Esc for at afbryde eller en anden tast for at foretage ny beregning");
+                        var tast = Console.ReadKey();
+                        if (tast.Key == ConsoleKey.Escape)
+                            break;
+
+                    } while (true);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Hov - der er sket en fejl (" + ex.Message + ")");
+                    return;
+                }
             }
         }
     }
